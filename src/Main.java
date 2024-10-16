d8405e625ee8ed1acf520565261c5668bcef9ee3
IpHo1 <ilya.cherny2007@gmail.com>
2024-10-16 22:07:53 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int ticketPrice = 13676; // стоимость билета
+        int milesPerRuble = 20;  // количество рублей для одной бонусной мили
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Рассчитываем количество бонусных миль
+        int bonusMiles = ticketPrice / milesPerRuble;
+
+        // Выводим результат
+        System.out.println("Стоимость билета: " + ticketPrice + " рублей");
+        System.out.println("Начисленные бонусные мили: " + bonusMiles + " миль");
     }
 }
